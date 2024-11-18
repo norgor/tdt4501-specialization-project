1dbe15b669e1f2b7659a9e139410de30ffc6ec11
magsteen <mag.steensland@gmail.com>
2024-11-18 11:25:36 +0100

    Applied python Black Formatter vscode extension

--- coalpaper.py
+++ coalpaper.py
@@ -14,6 +14,7 @@ from bs4 import BeautifulSoup
 from typing import Self
 from dataclasses import dataclass
 
+
 class RowIdent:
     def __init__(self, doi: str | None, issn: str | None, isbn: str | None):
         if doi:
@@ -33,17 +34,20 @@ class RowIdent:
         if self.type and self.value:
             return f"{self.type}:{self.value}"
         return ""
-    
+
     def __hash__(self) -> int:
         return hash((self.type, self.value))
 
     def __eq__(self, other: Self) -> bool:
-        if self.type is None or self.value is None or other.type is None or other.value is None:
+        if (
+            self.type is None
+            or self.value is None
+            or other.type is None
+            or other.value is None
+        ):
             return False
         return self.type == other.type and self.value == other.value
-        
 
-        
 
 @dataclass
 class Row:
@@ -58,50 +62,103 @@ class Row:
     year: str
 
     def to_csv_dict(self):
-        return dict(zip(Row.CSV_FIELDNAMES, (
-            f"{self.query_id}_{self.index+1}", self.ident, self.title, self.url, self.year
-        )))
-    
+        return dict(
+            zip(
+                Row.CSV_FIELDNAMES,
+                (
+                    f"{self.query_id}_{self.index+1}",
+                    self.ident,
+                    self.title,
+                    self.url,
+                    self.year,
+                ),
+            )
+        )
+
     def __canontitle(self) -> str:
-        return Row.CANONTITLE_REMOVECHARS.sub('', self.title).lower()
+        return Row.CANONTITLE_REMOVECHARS.sub("", self.title).lower()
 
     def __hash__(self) -> int:
         return hash(self.__canontitle())
 
     def __eq__(self, other) -> bool:
-        return isinstance(other, Row) and self.ident == other.ident or self.__canontitle() == other.__canontitle()
+        return (
+            isinstance(other, Row)
+            and self.ident == other.ident
+            or self.__canontitle() == other.__canontitle()
+        )
 
     def __str__(self):
         return f"[{self.ident}] {self.title} ({self.__canontitle()})"
 
+
 def scopus(data: io.TextIOWrapper, query_id: str) -> list[Row]:
     reader = csv.DictReader(data)
-    return [Row(query_id, index, row["Title"], RowIdent(row.get("DOI"), row.get("ISSN"), row.get("ISBN")), row["Link"], row["Year"]) for (index, row) in zip(itertools.count(), reader)]
+    return [
+        Row(
+            query_id,
+            index,
+            row["Title"],
+            RowIdent(row.get("DOI"), row.get("ISSN"), row.get("ISBN")),
+            row["Link"],
+            row["Year"],
+        )
+        for (index, row) in zip(itertools.count(), reader)
+    ]
+
 
 def ieee(data: io.TextIOWrapper, query_id: str) -> list[Row]:
     reader = csv.DictReader(data)
-    return [Row(query_id, index, row["Document Title"], RowIdent(row.get("DOI"), row.get("ISSN"), row.get("ISBNs")), row["PDF Link"], row["Publication Year"]) for (index, row) in zip(itertools.count(), reader)]
+    return [
+        Row(
+            query_id,
+            index,
+            row["Document Title"],
+            RowIdent(row.get("DOI"), row.get("ISSN"), row.get("ISBNs")),
+            row["PDF Link"],
+            row["Publication Year"],
+        )
+        for (index, row) in zip(itertools.count(), reader)
+    ]
+
 
 def acm_dl(data: io.TextIOWrapper, query_id: str) -> list[Row]:
     reader = bibtexparser.load(data)
-    return [Row(query_id, index, row["title"], RowIdent(row.get("doi"), row.get("issn"), row.get("isbn")), row.get("url"), row["year"]) for (index, row) in zip(itertools.count(), reader.entries)]
+    return [
+        Row(
+            query_id,
+            index,
+            row["title"],
+            RowIdent(row.get("doi"), row.get("issn"), row.get("isbn")),
+            row.get("url"),
+            row["year"],
+        )
+        for (index, row) in zip(itertools.count(), reader.entries)
+    ]
+
 
 def web_of_science(data: io.TextIOWrapper, query_id: str) -> list[Row]:
     reader = csv.DictReader(data, dialect="excel-tab")
-    return [Row(query_id, index, BeautifulSoup(row["TI"]).get_text(), RowIdent(row.get("DI"), row.get("SN"), row.get("BN")), f"https://www.webofscience.com/wos/woscc/full-record/{row["UT"]}", row["PY"]) for (index, row) in zip(itertools.count(), reader)]
+    return [
+        Row(
+            query_id,
+            index,
+            BeautifulSoup(row["TI"]).get_text(),
+            RowIdent(row.get("DI"), row.get("SN"), row.get("BN")),
+            f"https://www.webofscience.com/wos/woscc/full-record/{row["UT"]}",
+            row["PY"],
+        )
+        for (index, row) in zip(itertools.count(), reader)
+    ]
+
 
 def main(args: list[str]):
     # list of databases from most to least specific (for dedupe)
-    dbs = (
-        ("ACM", acm_dl),
-        ("IEEE", ieee),
-        ("SCP", scopus),
-        ("WOS", web_of_science)
-    )
+    dbs = (("ACM", acm_dl), ("IEEE", ieee), ("SCP", scopus), ("WOS", web_of_science))
     rows = []
     n_duplicates = 0
-    
-    for (prefix, func) in dbs:
+
+    for prefix, func in dbs:
         for name in glob.glob(f"queries/{prefix}_*"):
             query_id = os.path.splitext(os.path.split(name)[1])[0]
             print(f"Processing {name}...")
@@ -113,15 +170,14 @@ def main(args: list[str]):
                         continue
                     rows.append(row)
 
-    
     with open("coalpaper.csv", "w") as outfile:
         writer = csv.DictWriter(outfile, fieldnames=Row.CSV_FIELDNAMES)
         writer.writeheader()
         for row in rows:
             writer.writerow(row.to_csv_dict())
-    
+
     print(f"Removed {n_duplicates} duplicates")
-        
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
